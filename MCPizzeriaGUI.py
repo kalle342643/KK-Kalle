44425e8fb53079f8cad6fb25247f99d102599559
kalle342643 <114422@montessoricollege.nl>
2025-02-06 16:08:10 +0100

    opdracht 4.7.2.1

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -24,6 +24,16 @@ venster.wm_title("MC Pizzeria")
 labelIntro = Label(venster, text="Welkom!")
 labelIntro.grid(row=0, column=0, sticky="W")
 
+labelIntro = Label(venster, text="Klantnaam!")
+labelIntro.grid(row=1, column=0, sticky="W")
+
+ingevoerde_klantnaam = StringVar()
+invoerveldKlantnaam = Entry(venster, textvariable=ingevoerde_klantnaam)
+invoerveldKlantnaam.grid(row=1, column=1, sticky="W")
+
+invoerveldKlantNr = Entry(venster)
+invoerveldKlantNr.grid(row=2, column=1, sticky="W")
+
 knopSluit = Button(venster, text="Sluiten", width=12, command=venster.destroy)
 knopSluit.grid(row=17, column=4)
 
