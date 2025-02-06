0783e6af82151a7c7868d55a1c2dc757cff4174f
kalle342643 <114422@montessoricollege.nl>
2025-02-06 16:07:28 +0100

    4.7.2.2

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -19,8 +19,17 @@ import MCPizzeriaSQL
 ### --------- Hoofdprogramma  ---------------
 
 venster = Tk()
-venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
 venster.wm_title("MC Pizzeria")
+
+labelIntro = Label(venster, text="Welkom!")
+labelIntro.grid(row=0, column=0, sticky="W")
+
+knopSluit = Button(venster, text="Sluiten", width=12, command=venster.destroy)
+knopSluit.grid(row=17, column=4)
+
+#reageert op gebruikersinvoer, deze regel als laatste laten staan
+venster.mainloop()
+
 
 
 
