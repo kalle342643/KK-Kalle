10e5439f724abdfb1bf333e76d02aa8b2de2d165
kalle342643 <114422@montessoricollege.nl>
2025-03-20 09:59:52 +0100

    giu

--- KKfilmsGUI.py
+++ KKfilmsGUI.py
@@ -16,12 +16,26 @@ import KKfilmsSQL
 ### ---------  Functie definities  -----------------
 
 ### --------- Hoofdprogramma  ---------------
-
-
-
-
-venster = Tk()
+venster = Tk ()
 venster.wm_title("KK Moviedatabase")
-venster.iconbitmap("IMDB logo.ico")
-venster.config(bg="yellow")
+venster.iconbitmap("KK.ico")
+venster.config(bg="#ffde59")
+venster.attributes('-fullscreen', True)
 
+labelIntro = Label(venster, text="welcome!")
+labelIntro.grid(row=0, column=0, sticky="W")
+
+labelIntro = Label(venster, text="Movies:!")
+labelIntro.grid(row=1, column=0, sticky="W")
+
+ingevoerde_movies = StringVar()
+invoerveldmoviesname = Entry(venster, textvariable=ingevoerde_movies)
+invoerveldmoviesname.grid(row=1, column=1, sticky="W")
+
+knopSluit = Button(venster, text="close", width=12, command=venster.destroy)
+knopSluit.grid(row=17, column=4)
+
+
+
+
+venster.mainloop()
