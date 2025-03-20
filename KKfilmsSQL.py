3cb05a3d9c088a624cc7c455e1b770516d81be6c
kalle342643 <114422@montessoricollege.nl>
2025-03-20 09:16:54 +0100

    gui

--- KKfilmsSQL.py
+++ KKfilmsSQL.py
@@ -1,8 +1,10 @@
 # Vul hier de naam van je programma in:
-# KKfilms
+# kkmdb
 #
 # Vul hier jullie namen in:
 # Kalle en Kimo
+#
+#
 
 
 ### --------- Bibliotheken en globale variabelen -----------------
@@ -14,18 +16,11 @@ with sqlite3.connect("Kkfilms.db") as db:
 def maakTabellenAan ():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tbl_actors(
-        Actor_id INTEGER NOT NULL 
-        Movie_id INTEGER NOT NULL  );""")
-    print("Tabel 'tbl_actors' aangemaakt.")
-
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS tbl_roles(
         Actor_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        Actor_birth INTEGER NOT NULL,
         First_name TEXT NOT NUll,
-        Last_name TEXT NOT NULL);""")
+        Last_name TEXT NOT NULL,
+        Actor_birth INTEGER NOT NULL);""")
     print("Tabel 'tbl_actors' aangemaakt.")
-    
 
 
 def printTabel(tbl_actors): 
